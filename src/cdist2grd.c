6a9311af3dd5c8e7ffebdfcdff707ce9ff94e86b
Thomas Kleiner <tkleiner@awi.de>
2020-11-11 22:38:34 +0100

    clang-format

--- src/cdist2grd.c
+++ src/cdist2grd.c
@@ -5,16 +5,15 @@
 #include <config.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <getopt.h>
 #include <math.h>
 #include <netcdf.h>
-#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "debug_printf.h"
 #include "gmtopts.h"
 #include "grdio.h"
-#include "debug_printf.h"
-
 
 /**
  * print usage information and exit
@@ -26,8 +25,6 @@ static void usage(void);
  */
 static void version(void);
 
-
-
 /* for logging */
 const char *program_name = "cdist2grd";
 
@@ -36,210 +33,206 @@ const char *program_name = "cdist2grd";
  * ixy = j * nx + i;
  */
 #ifndef IJ2IND
-#define IJ2IND(i,j,nx) (((size_t)(j))*((size_t)(nx))+(size_t)(i))
+#define IJ2IND(i, j, nx) (((size_t)(j)) * ((size_t)(nx)) + (size_t)(i))
 #endif
 
-
 #ifndef MIN
-#define MIN(X,Y) ( ((X) < (Y)) ? (X) : (Y) )
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 #endif
 
 #ifndef MAX
-#define MAX(X,Y) ( ((X) > (Y)) ? (X) : (Y) )
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 #endif
 
 /*****************************************************************************/
-int main( int argc, char** argv )
-{
+int main(int argc, char **argv) {
 
-  double *p_x=NULL;
-  double *p_y=NULL;
-  double *p_grd=NULL;
+  double *p_x = NULL;
+  double *p_y = NULL;
+  double *p_grd = NULL;
 
-  char* p_file_name = NULL;
-  char* p_grd_name = NULL;
+  char *p_file_name = NULL;
+  char *p_grd_name = NULL;
 
-  FILE* fp = NULL;
+  FILE *fp = NULL;
   char line[BUFSIZ];
 
-  int oc,err=0;
-  int R_opt = 0; 
+  int oc, err = 0;
+  int R_opt = 0;
   int I_opt = 0;
   int verbose = 0;
 
-  double xmin=0.0, xmax=0.0, xinc=0.0, xlen = 0.0, xpos = 0.0, xdist = 0.0;
-  double ymin=0.0, ymax=0.0, yinc=0.0, ylen = 0.0, ypos = 0.0, ydist = 0.0;
+  double xmin = 0.0, xmax = 0.0, xinc = 0.0, xlen = 0.0, xpos = 0.0,
+         xdist = 0.0;
+  double ymin = 0.0, ymax = 0.0, yinc = 0.0, ylen = 0.0, ypos = 0.0,
+         ydist = 0.0;
   double dist = 0.0;
   size_t nx = 0, ny = 0, nn = 0;
   size_t i = 0, j = 0, k = 0;
-  unsigned int nlines=0;
-
-  
+  unsigned int nlines = 0;
 
   /* parse commandline args */
-  while ((oc = getopt (argc, argv, "VhR:I:v")) != -1)
+  while ((oc = getopt(argc, argv, "VhR:I:v")) != -1)
     switch (oc) {
-    case 'R': 
+    case 'R':
       /* Region */
-			R_opt = 1;
-			parse_R_option((char *)optarg, &xmin, &xmax, &ymin, &ymax);
+      R_opt = 1;
+      parse_R_option((char *)optarg, &xmin, &xmax, &ymin, &ymax);
       break;
-    case 'I': 
+    case 'I':
       /* incerement */
-			I_opt = 1;
-			parse_I_option((char *)optarg, &xinc, &yinc);
+      I_opt = 1;
+      parse_I_option((char *)optarg, &xinc, &yinc);
       break;
-    case 'h': 
+    case 'h':
       /* help */
-      usage(); break;
+      usage();
+      break;
     case 'v':
       /* version */
-      version(); break;
-    case 'V': 
+      version();
+      break;
+    case 'V':
       /* verbose opttion */
-      verbose++; break;
+      verbose++;
+      break;
     case '?':
-      fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+      fprintf(stderr, "Unknown option `-%c'.\n", optopt);
       return 1;
     default:
-      abort ();
+      abort();
     }
 
-
   if ((argc - optind) == 1) {
-    p_grd_name = (char*)argv[optind];
+    p_grd_name = (char *)argv[optind];
   } else if ((argc - optind) == 2) {
-    p_file_name = (char*)argv[optind];
-    p_grd_name = (char*)argv[optind+1];
+    p_file_name = (char *)argv[optind];
+    p_grd_name = (char *)argv[optind + 1];
   } else {
     usage();
   }
 
   xlen = xmax - xmin;
   ylen = ymax - ymin;
-  if (xlen*ylen <= 0.0) {
-    fprintf(stderr,"xmin: %.3f xmax: %.3f\n", xmin,xmax);
-    fprintf(stderr,"ymin: %.3f ymax: %.3f\n", ymin,ymax);
+  if (xlen * ylen <= 0.0) {
+    fprintf(stderr, "xmin: %.3f xmax: %.3f\n", xmin, xmax);
+    fprintf(stderr, "ymin: %.3f ymax: %.3f\n", ymin, ymax);
     return EXIT_FAILURE;
   }
 
-  if (remainder(xlen,xinc) != 0.0) {
-    fprintf(stderr,"xlen: %.3f xinc: %.3f\n", xlen,xinc);
+  if (remainder(xlen, xinc) != 0.0) {
+    fprintf(stderr, "xlen: %.3f xinc: %.3f\n", xlen, xinc);
     return EXIT_FAILURE;
-    
+
   } else {
     nx = (size_t)(xlen / xinc) + 1;
   }
-    
-  if (remainder(ylen,yinc) != 0.0) {
-    fprintf(stderr,"ylen: %.3f yinc: %.3f\n", ylen,yinc);
+
+  if (remainder(ylen, yinc) != 0.0) {
+    fprintf(stderr, "ylen: %.3f yinc: %.3f\n", ylen, yinc);
     return EXIT_FAILURE;
   } else {
     ny = (size_t)(ylen / yinc) + 1;
   }
 
-  
   if (verbose) {
-    fprintf(stderr,"Input:\n");
-    fprintf(stderr,"xmin: %.3f xmax: %.3f x_inc: %f nx: %lu\n",
-            xmin,xmax,xinc,nx);
-    fprintf(stderr,"ymin: %.3f ymax: %.3f y_inc: %f ny: %lu\n",
-            ymin,ymax,yinc,ny);
-    fprintf(stderr,"verbose: %u\n",verbose);
+    fprintf(stderr, "Input:\n");
+    fprintf(stderr, "xmin: %.3f xmax: %.3f x_inc: %f nx: %lu\n", xmin, xmax,
+            xinc, nx);
+    fprintf(stderr, "ymin: %.3f ymax: %.3f y_inc: %f ny: %lu\n", ymin, ymax,
+            yinc, ny);
+    fprintf(stderr, "verbose: %u\n", verbose);
   }
 
-
-  
   p_x = (double *)calloc(nx, sizeof(double));
-  for(k=0; k<nx; k++) p_x[k] = k*xinc + xmin;
-  
+  for (k = 0; k < nx; k++)
+    p_x[k] = k * xinc + xmin;
+
   p_y = (double *)calloc(ny, sizeof(double));
-  for(k=0; k<ny; k++) p_y[k] = k*yinc + ymin;
-  
+  for (k = 0; k < ny; k++)
+    p_y[k] = k * yinc + ymin;
+
   nn = ny * nx; /* number of points in grd */
-	p_grd = (double *)calloc(nn, sizeof(double));
-  for(k=0; k<nn; k++) {
+  p_grd = (double *)calloc(nn, sizeof(double));
+  for (k = 0; k < nn; k++) {
     p_grd[k] = 9.999e32;
   }
-  
 
-  
-  if (p_file_name == NULL) {   /* Just read standard input */
+  if (p_file_name == NULL) { /* Just read standard input */
     fp = stdin;
-    if (verbose) fprintf (stderr, "Reading from standard input\n");
+    if (verbose)
+      fprintf(stderr, "Reading from standard input\n");
   } else {
-    if ( (fp = fopen (p_file_name, "r")) == NULL) {
-      fprintf (stderr, "Cannot open file %s\n",p_file_name);
+    if ((fp = fopen(p_file_name, "r")) == NULL) {
+      fprintf(stderr, "Cannot open file %s\n", p_file_name);
       return EXIT_FAILURE;
     } else {
-      if(verbose) fprintf (stderr, "Working on file %s\n", p_file_name);
+      if (verbose)
+        fprintf(stderr, "Working on file %s\n", p_file_name);
     }
   }
 
-  
-  while (!feof(fp) && (fgets (line, BUFSIZ, fp) != 0) ) {
-    
+  while (!feof(fp) && (fgets(line, BUFSIZ, fp) != 0)) {
+
     nlines++;
     if (verbose) {
-      fprintf(stderr,"Processing input line: %u ...\n",nlines);
+      fprintf(stderr, "Processing input line: %u ...\n", nlines);
     }
-    
-    if( line[0] == '#' || line[0] == '>' || line[0]== '\n' ) {
+
+    if (line[0] == '#' || line[0] == '>' || line[0] == '\n') {
       continue; /* continue with next point in file */
     }
-    
-    if(2 != sscanf (line, "%lf %lf", &xpos,&ypos)){
-      fprintf(stderr,
-              "ERROR: Mismatch between actual and expected fields near line %u\n",
-              nlines);
+
+    if (2 != sscanf(line, "%lf %lf", &xpos, &ypos)) {
+      fprintf(
+          stderr,
+          "ERROR: Mismatch between actual and expected fields near line %u\n",
+          nlines);
       return EXIT_FAILURE;
     }
-    
+
     if (verbose) {
-      fprintf(stderr,"       x0=%.3f y0=%.3f\n",xpos,ypos);
+      fprintf(stderr, "       x0=%.3f y0=%.3f\n", xpos, ypos);
     }
-    
-    
+
     /*****************************************************************
      * the work is starting here
      ******************************************************************/
 
-    for (j=0; j < ny; j++) {
+    for (j = 0; j < ny; j++) {
       ydist = abs(ypos - p_y[j]);
 
-      for (i=0; i < nx; i++) {
+      for (i = 0; i < nx; i++) {
         xdist = abs(xpos - p_x[i]);
-        dist = sqrt(xdist*xdist + ydist*ydist);
-        k = IJ2IND(i,j,nx);
-        p_grd[k] = MIN(p_grd[k],dist);
+        dist = sqrt(xdist * xdist + ydist * ydist);
+        k = IJ2IND(i, j, nx);
+        p_grd[k] = MIN(p_grd[k], dist);
       }
     }
-    
-    
   }
-  
+
   /* write blank array  */
-  grdwrite (p_grd_name, nx, ny, p_x, p_y, (void *)p_grd, NC_DOUBLE);
-  
-  
-  if (fp != stdin) fclose(fp);
-  if(p_x) free(p_x);
-  if(p_y) free(p_y);
-  if(p_grd) free(p_grd);
-  return  EXIT_SUCCESS;
+  grdwrite(p_grd_name, nx, ny, p_x, p_y, (void *)p_grd, NC_DOUBLE);
+
+  if (fp != stdin)
+    fclose(fp);
+  if (p_x)
+    free(p_x);
+  if (p_y)
+    free(p_y);
+  if (p_grd)
+    free(p_grd);
+  return EXIT_SUCCESS;
 } /* main */
-
-
-
-
 
 /**
  *
  */
-void usage(void)
-{
-  fprintf(stderr,"NAME:\n"
-          "  %s - generate stream lines based on GMT grid files\n",program_name);
+void usage(void) {
+  fprintf(stderr,
+          "NAME:\n"
+          "  %s - generate stream lines based on GMT grid files\n",
+          program_name);
 #if 0
   fprintf(stderr,"USAGE:\n"
           "  %s v_x.grd v_y.grd -f xyfile \n\n"
@@ -262,16 +255,15 @@ void usage(void)
   fprintf(stderr,"\nEXAMPLE:\n"
           "  echo \"0 0\" | %s vx.grd vy.grd | psxy -m -R -J ... \n"
           "  \n",program_name);
-#endif  
+#endif
   exit(0);
 }
-
 
 /**
  *
  */
 void version(void) {
-  fprintf(stderr,"This is %s version %s (%s).\n",
-          PACKAGE_NAME,PACKAGE_VERSION, __DATE__);
+  fprintf(stderr, "This is %s version %s (%s).\n", PACKAGE_NAME,
+          PACKAGE_VERSION, __DATE__);
   exit(0);
 }
